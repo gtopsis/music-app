62138985149d877c4862be94d35a8e4e81936113
Giorgos Topsis <giorgostopsis@protonmail.com>
2021-08-12 18:27:02 +0300

    fix: error handler (middleware)

--- src/middlewares/response-error.js
+++ src/middlewares/response-error.js
@@ -2,10 +2,9 @@ module.exports = (err, req, res, next) => {
   let status = err.status || 500;
   let payload = {
     message: err.message || "Server Error",
+    error: err.errors || [],
   };
 
   res.status = status;
-  res.json({
-    error: payload,
-  });
+  res.json(payload);
 };
