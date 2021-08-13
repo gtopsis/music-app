1afabe55327f635927bc4ad1219f426b3747eacc
Giorgos Topsis <giorgostopsis@protonmail.com>
2021-08-13 21:07:38 +0300

    refactor: clean testSetupService

--- tests/testSetupService.js
+++ tests/testSetupService.js
@@ -1,23 +1,9 @@
 // test-setup.js
 const appConfig = require("../src/config");
-const request = require("supertest");
 const logger = require("../src/logger");
 const dbService = require("../src/services/dbService");
 
 async function dropAllCollections() {}
-
-async function DBConnect() {
-  dbService.init();
-
-  // if (USERNAME) {
-  //   if (USER_PWD) {
-  //     USER_CREDENTIALS = `${USERNAME}:${USER_PWD}`;
-  //   } else {
-  //     USER_CREDENTIALS = `${USERNAME}`;
-  //   }
-  //   USER_CREDENTIALS_URI_PART = `${USER_CREDENTIALS}@`;
-  // }
-}
 
 module.exports = {
   setUpTestEnv(app) {
@@ -25,11 +11,7 @@ module.exports = {
     beforeAll(async () => {
       process.env.NODE_ENV = "test";
 
-      try {
-        await DBConnect();
-      } catch (error) {
-        logger.error(error);
-      }
+      dbService.init();
     });
 
     // // Cleans up database between each test
