5b79be0b45b70f8a993f5203edcbe01c60b46a34
Giorgos Topsis <giorgostopsis@protonmail.com>
2021-08-13 19:42:49 +0300

    test: basic test suites for artists-routes

---
+++ tests/artists-routes.test.js
@@ -0,0 +1,54 @@
+const request = require("supertest");
+const app = require("../src/app");
+
+describe("POST /v1/artists", () => {
+  it("Create an artist successfully", async () => {
+    const response = await request(app)
+      .post("/v1/artists")
+      .send({name: "john"})
+      .set("Accept", "application/json")
+      // .expect("Content-Type", /json/)
+      .expect(200);
+  });
+});
+
+describe("GET /v1/artists", () => {
+  it("Retrieve all artists successfully", async () => {
+    const response = await request(app)
+      .get("/v1/artists")
+      .set("Accept", "application/json")
+      // .expect("Content-Type", /json/)
+      .expect(200);
+  });
+});
+
+describe("PATCH /v1/artists/:id", () => {
+  it("Update an Artist successfully", async () => {
+    const response = await request(app)
+      .patch("/v1/artists")
+      .send({name: "john"})
+      .set("Accept", "application/json")
+      // .expect("Content-Type", /json/)
+      .expect(200);
+  });
+});
+
+describe("GET /v1/artists/:id", () => {
+  it("Retrieve an Artist successfully", async () => {
+    const response = await request(app)
+      .get("/v1/artists")
+      .set("Accept", "application/json")
+      // .expect("Content-Type", /json/)
+      .expect(200);
+  });
+});
+
+describe("DELETE /v1/artists/:id", () => {
+  it("Delete an Artist successfully", async () => {
+    const response = await request(app)
+      .delete("/v1/artists")
+      .set("Accept", "application/json")
+      // .expect("Content-Type", /json/)
+      .expect(200);
+  });
+});
