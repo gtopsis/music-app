61a06053b840f9651756d61dab8cd77df8c81f73
Giorgos Topsis <giorgostopsis@protonmail.com>
2021-08-15 04:17:45 +0300

    refactor: load node env from app's config file

--- src/models/index.js
+++ src/models/index.js
@@ -4,8 +4,9 @@ const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
+const appConfig = require("../config");
 const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/dbConfig.js")[env];
+const config = require(__dirname + "/../config/dbConfig.js")[appConfig.ENVIRONMENT];
 const db = {};
 
 let sequelize;
