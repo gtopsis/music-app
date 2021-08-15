feba3e8b703a51d3817c283f46f99bc96cbaf5bf
Giorgos Topsis <giorgostopsis@protonmail.com>
2021-08-15 19:00:23 +0300

    docs: validate path params as valid uuid
    added custom validation handler

--- src/utils/openapi-data-formats.js
+++ src/utils/openapi-data-formats.js
@@ -24,5 +24,17 @@ module.exports = {
         return secondsInt >= 0 && secondsInt < 60 && minutesInt >= 0 && minutesInt < 60 && hoursInt >= 0;
       },
     },
+    {
+      name: "uuid",
+      type: "string",
+      validate: v => {
+        if (v == undefined || v == null || v == "") return false;
+
+        // Regular expression to check if string is a valid UUID
+        const regexExp = /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi;
+
+        return regexExp.test(v);
+      },
+    },
   ],
 };
